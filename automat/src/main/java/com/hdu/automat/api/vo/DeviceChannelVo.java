2432090f1608978c98b8cb3be3b7f1ae126bc989
Ranly <1193932296@qq.com>
2021-07-04 15:27:40 +0800

    fix

--- automat/src/main/java/com/hdu/automat/api/vo/DeviceChannelVo.java
+++ automat/src/main/java/com/hdu/automat/api/vo/DeviceChannelVo.java
@@ -18,14 +18,8 @@ public class DeviceChannelVo implements Serializable {
     // 通道名称
     private String channelName;
 
-    // 商品Id
-    private Long itemId;
-
     // 通道商品名称
     private String itemName;
-
-    // 剩余量
-    private Integer volume;
 
     // 货量描述  eg: 红豆薏米茶 10
     private String volumeDesc;
